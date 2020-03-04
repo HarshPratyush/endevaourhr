9a9db9194d056b804d1578c8363d3b366da01358
Harsh Pratyush <harshpratyush1206@gmail.com>
2020-03-05 01:09:51 +0530

    Home Page Changed

--- src/app/static/home/home.component.ts
+++ src/app/static/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 declare var $:any;
+import { interval } from 'rxjs';
+import { element } from 'protractor';
 
 @Component({
   selector: 'najah-home',
@@ -8,22 +10,49 @@ declare var $:any;
 })
 export class HomeComponent implements OnInit {
 
+
+  divArray:string[]=['customer','jobs','resume'];
   constructor() { 
 
-    $('.count').each(function () {
-      $(this).prop('Counter',0).animate({
-          Counter: $(this).text()
-      }, {
-          duration: 4000,
-          easing: 'swing',
-          step: function (now) {
-              $(this).text(Math.ceil(now));
-          }
-      });
-  });
   }
 
   ngOnInit() {
+
+    const secondsCounter = interval(5000);
+    let i =0;
+
+    secondsCounter.subscribe(n =>{
+      document.getElementById(this.divArray[i]).classList.remove('colored');
+      if(i==this.divArray.length-1){
+        i=0;
+      }
+      else{
+        i=i+1;
+      }
+      let a = this.divArray[i];
+      document.getElementById(a).classList.add('colored');
+    });
+
+
+    $('.counter').each(function () {
+      $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+      }, {
+      duration: 5000,
+      easing: 'swing',
+      step: function (now) {
+      $(this).text(Math.ceil(now));
+      }
+      });
+      });
+
+      $('.swing-counter').on('inview', function (event, visible) {
+        if (visible == true) {
+          alert("reached")
+        } else {
+          // element has gone out of viewport
+        }
+      });
   }
 
 }
